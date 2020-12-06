9ef0d4c0c66143e552a5110f695aa5b4cd661374
hk38 <160441038@ccalumni.meijo-u.ac.jp>
2020-12-06 12:31:23 +0900

    アプリ起動時のRealmの設定を記述

--- app/src/main/java/lit/amida/realmrecyclerviewsample/RealmApp.kt
+++ app/src/main/java/lit/amida/realmrecyclerviewsample/RealmApp.kt
@@ -1,4 +1,17 @@
 package lit.amida.realmrecyclerviewsample
 
-class RealmApp {
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class RealmApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Realm.init(this)
+        val realmConfig = RealmConfiguration.Builder()
+            // 保存しているクラスの構造に変更があればRealmに保存したデータを全部削除してから更新する
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfig)
+    }
 }
